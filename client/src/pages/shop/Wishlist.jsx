cb2b8ab52ab897567e9fa3fcc39d3a3cf8a4a95a
BikashChamua01 <bikashchamua537@gmail.com>
2025-07-26 14:06:59 +0530

    Small change in the wishlist page | made grid-col-4

--- client/src/pages/shop/Wishlist.jsx
+++ client/src/pages/shop/Wishlist.jsx
@@ -50,7 +50,7 @@ const Wishlist = ({ user }) => {
             <p className="text-gray-500">Your wishlist is currently empty.</p>
           )}
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
             <AnimatePresence>
               {!loading &&
                 wishlist.map((product, index) => (
