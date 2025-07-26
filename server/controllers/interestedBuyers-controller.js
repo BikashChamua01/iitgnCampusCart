400124aa96d753960916194cca51832eb221306e
BikashChamua01 <bikashchamua537@gmail.com>
2025-07-26 11:31:57 +0530

    Get Buyer request in the seller side, Back-end done

--- server/controllers/interestedBuyers-controller.js
+++ server/controllers/interestedBuyers-controller.js
@@ -75,4 +75,49 @@ const markInterested = async (req, res) => {
   }
 };
 
-module.exports = { markInterested };
+const getBuyRequests = async (req, res) => {
+  try {
+    const { userId, isAdmin } = req.user;
+    const { productId, sellerId } = req.body;
+    if (userId.toString() != sellerId.toString() && !isAdmin)
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        success: false,
+        msg: "You are not authorized to access this ",
+      });
+
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        msg: "This product doesn't exists",
+      });
+    }
+
+    const requests = await InterestedBuyers.findOne({ sellerId });
+    //     Now find that particular product in the requests
+    const productIndex = requests.products.findIndex(
+      (product) => product.productId.toString() === productId.toString()
+    );
+
+    if (productIndex === -1 || requests.products.length === 0)
+      return res.status(StatusCodes.OK).json({
+        success: true,
+        msg: "No users has shown interested for this product",
+        buyRequests: [],
+      });
+
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      msg: `${requests.products[productIndex].buyers.length} user(s) have shown interest to your product`,
+      buyRequests: [...requests.products[productIndex].buyers],
+    });
+  } catch (error) {
+    console.error("Error in markInterested:", error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      msg: "Failed to get the buy requests",
+    });
+  }
+};
+
+module.exports = { markInterested, getBuyRequests };
