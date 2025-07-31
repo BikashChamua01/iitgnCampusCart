eef6501bef0eb7b12a44223b6d8a0d4fa21c8ee2
BikashChamua01 <bikashchamua537@gmail.com>
2025-07-31 23:32:45 +0530

    Some styling changes to make it responsive, in the home page the tabs were mismatched for the md screen and the categories were overflowed in the home page md screen| tab style change in the account page

--- client/src/pages/shop/Products.jsx
+++ client/src/pages/shop/Products.jsx
@@ -20,7 +20,6 @@ import { Button } from "@/components/ui/button";
 import { SortDropDown } from "@/components/shop/Filter";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@radix-ui/react-dropdown-menu";
-import { useFetcher } from "react-router-dom";
 import axios from "axios";
 import { toast } from "sonner";
 
@@ -147,7 +146,7 @@ const ShopProducts = () => {
   };
 
   return (
-    <div className="min-h-screen  py-4  px-4 w-full relative">
+    <div className="pt-4  px-4 w-full relative">
       <div className="max-w-7xl mx-auto ">
         {/* Toolbar */}
         <div className="flex  items-center  bg-white  py-2 px-4 md:px-10 mb-8 border-b-1 border-violet-100 md:gap-6 z-50">
