70a38ab696a12fa316af2a9422ee4efca06ad57f
BikashChamua01 <bikashchamua537@gmail.com>
2025-08-05 21:15:21 +0530

    Small changes

--- client/src/components/shop/ConfirmDialogButton.jsx
+++ client/src/components/shop/ConfirmDialogButton.jsx
@@ -11,24 +11,24 @@ import {
 } from "../ui/alert-dialog";
 import { FaTrash } from "react-icons/fa";
 
-const ConfirmDialog = ({ onConfirm, msg,title, loading, setLoading }) => {
+const ConfirmDialog = ({ onConfirm, msg, title, loading }) => {
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
-       { title=="Delete"?
-        <button
-          className="flex-1 py-1 text-xs font-medium rounded-full
+        {title == "Delete" ? (
+          <button
+            className="flex-1 py-1 text-xs font-medium rounded-full
           bg-red-100 text-red-600
           hover:bg-red-200 hover:shadow-lg hover:shadow-red-200
           transition-all duration-200
           focus:outline-none focus:ring-2 focus:ring-red-300
           cursor-pointer flex items-center justify-center gap-1 w-full"
-        >
-          <FaTrash className="text-xs" /> Delete
-        </button>:<button className="custom-button outline-none">
-                        {title}
-                      </button>
-        }
+          >
+            <FaTrash className="text-xs" /> Delete
+          </button>
+        ) : (
+          <button className="custom-button outline-none">{title}</button>
+        )}
       </AlertDialogTrigger>
 
       <AlertDialogContent
@@ -49,34 +49,34 @@ const ConfirmDialog = ({ onConfirm, msg,title, loading, setLoading }) => {
         </AlertDialogHeader>
 
         <AlertDialogFooter className="mt-4 flex justify-end gap-2">
-  <AlertDialogCancel
-    disabled={loading}
-    className={`
+          <AlertDialogCancel
+            disabled={loading}
+            className={`
       px-4 py-2 text-sm font-medium rounded-md border border-gray-300
       text-gray-700 transition cursor-pointer
-      ${loading ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-100'}
+      ${loading ? "opacity-50 cursor-not-allowed" : "hover:bg-gray-100"}
     `}
-  >
-    Cancel
-  </AlertDialogCancel>
+          >
+            Cancel
+          </AlertDialogCancel>
 
-  <AlertDialogAction
-    disabled={loading}
-    onClick={onConfirm}
-    className={`
+          <AlertDialogAction
+            disabled={loading}
+            onClick={onConfirm}
+            className={`
       not-first:px-4 py-2 text-sm font-medium rounded-md
       transition cursor-pointer
-      ${title === "Accept" 
-        ? 'bg-green-600 text-white hover:bg-green-700' 
-        : 'bg-red-600 text-white hover:bg-red-700'}
-      ${loading ? 'opacity-50 cursor-not-allowed' : ''}
+      ${
+        title === "Accept"
+          ? "bg-green-600 text-white hover:bg-green-700"
+          : "bg-red-600 text-white hover:bg-red-700"
+      }
+      ${loading ? "opacity-50 cursor-not-allowed" : ""}
     `}
-  >
-    {loading ? "Processing..." : `Yes, ${title}`}
-  </AlertDialogAction>
-</AlertDialogFooter>
-
-
+          >
+            {loading ? "Processing..." : `Yes, ${title}`}
+          </AlertDialogAction>
+        </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
   );
