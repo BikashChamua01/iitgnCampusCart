5f336799a03a5f00635e0ce08a2badcc8f1162a9
BikashChamua01 <bikashchamua537@gmail.com>
2025-08-24 17:52:38 +0530

    Deployment config changes

--- server/api/index.js
+++ server/api/index.js
@@ -5,15 +5,19 @@ const connectDb = require("../db/connect");
 
 let connectionPromise = null;
 
+// Wrap the express app with serverless-http
 const handler = serverless(app);
 
 module.exports = async (req, res) => {
+  // Cache the DB connection promise across invocations to avoid reconnect storms
   if (!connectionPromise) {
     connectionPromise = connectDb(process.env.MONGO_URI).catch((err) => {
+      // reset so next invocation can retry
       connectionPromise = null;
       throw err;
     });
   }
   await connectionPromise;
+  // delegate to serverless handler
   return handler(req, res);
 };
