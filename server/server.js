05f8e9558e865c6c039cddebf71e38a8744fee2e
BikashChamua01 <bikashchamua537@gmail.com>
2025-08-24 16:57:08 +0530

    cookieparser

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-// server.js (local dev)
+const express = require("express");
+const app = express();
 require("dotenv").config();
 const connectDb = require("./db/connect");
-const app = require("./app");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const PORT = process.env.PORT || 5000;
-
 
 // Built in middlewares
 app.use(cookieParser());
@@ -42,17 +43,14 @@ app.use("/api/v1/wishlist", wishlistRouter);
 app.use("/api/v1/interested", interestedBuyerRouter);
 
 // Connection to the database and start the server
-
 async function start() {
   try {
+    // First connect to the db
     await connectDb(process.env.MONGO_URI);
-    app.listen(PORT, () => console.log("App running on port " + PORT));
+    app.listen(PORT, console.log("App is running in " + PORT));
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
 }
 
-// Only start when run directly
-//if (require.main === module) {
-  start();
-//}
+start();
